62c20f508b942373c968f7a331a5c48f5619f21d
sheliakst <stamcj@hotmail.com>
2019-03-26 17:12:23 +0200

    new animations#rect4752#rect4752#rect4752

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    setTimeout(function(){ $("#rect4677,#path4799").css('display','none'); },14000);
+
+    setTimeout(function(){ $("#rect4752").css('display','none'); },47000);
+
+
+
+})
